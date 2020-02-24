9b3a39949d2612bae6e132e545337cfafecd41c5
ManuelBouza <42628138+ManuelBouza@users.noreply.github.com>
2020-02-23 21:12:53 -0500

    making edit on Wizard

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models, rulers_generator, rulers, operators
+from . import models, rulers_generator, rulers, operators, ruler_wizard
--- models/rulers.py
+++ models/rulers.py
@@ -108,6 +108,46 @@ class Rulers(models.Model):
 
     @api.multi
     def edit(self):
+        fields_type = self.fields_type.encode('ascii', 'ignore')
+        #
+        # ruler_values = {
+        #     'date': self.date_value,
+        #     'datetime': self.date_time_value,
+        #     'integer': self.integer_value,
+        #     'boolean': self.bool_value,
+        #     'char': self.char_value,
+        #     'float': self.float_value,
+        #     'monetary': self.date_value,
+        #     'many2many': self.date_value,
+        #     'many2one': self.many2many_value,
+        #     'selection': self.many2many_value,
+        # }
+        #
+        # ruler_value = ruler_values[fields_type]
+
+        #     if fields_type == 'date':
+        #         ruler["date_value"] = self.date_value
+        #
+        #     elif fields_type == 'datetime':
+        #         ruler["date_time_value"] = self.date_time_value
+        #
+        #     elif fields_type == 'integer':
+        #         ruler["integer_value"] = self.integer_value
+        #
+        #     elif fields_type == 'boolean':
+        #         ruler["bool_value"] = self.bool_value
+        #
+        #     elif fields_type == 'char':
+        #         ruler["char_value"] = self.char_value
+        #
+        #     elif fields_type in ['float', 'monetary']:
+        #         ruler["float_value"] = self.float_value
+        #
+        #     elif fields_type in [' many2many', 'many2one']:
+        #         ruler["many2many_value"] = self.many2many_value
+        # else:
+        #     return False
+
         ruler_wizard = self.env['ruler.wizard'].create(
             {
                 'name': "Temp Name!!",
@@ -155,87 +195,3 @@ class TransientSelection(models.TransientModel):
         string='Key_id',
         required=False,
     )
-
-
-class RulerWizard(models.TransientModel):
-    _name = 'ruler.wizard'
-    _description = 'Ruler Wizard'
-    _inherit = 'rulers'
-
-    @api.depends('ruler_field')
-    def _compute_fields_type(self):
-        if self.ruler_field:
-            self.fields_type = self.ruler_field.ttype
-
-    @api.onchange('ruler_field')
-    def onchange_method(self):
-
-        if self.fields_type:
-            self.fields_type = self.ruler_field.ttype
-
-        if self.fields_type in ['selection', 'many2many', 'many2one']:
-            self.many2many_value = False
-
-            transient_ids = []
-
-            if self.fields_type == 'selection':
-                ruler_model = self.ruler_model.model
-                ruler_field = self.ruler_field.name
-
-                selection_values = self.env[ruler_model]._fields[ruler_field].selection
-
-                for value in selection_values:
-                    key_id = value[0]
-                    name = value[1]
-
-                    transient = self.env['transient.selection'].create(
-                        {
-                            'name': name,
-                            'key_id': key_id,
-                        }
-                    )
-
-                    transient_ids.append(transient.id)
-            else:
-
-                model_id = self.ruler_field.relation  # esto es el modelo
-                records = self.env[model_id].search([])
-
-                for record in records:
-                    transient = self.env['transient.selection'].create(
-                        {
-                            'name': record.name,
-                            'model_id': record.id,
-                        }
-                    )
-
-                    transient_ids.append(transient.id)
-
-            return {'domain': {'many2many_value': [('id', 'in', transient_ids)]}}
-
-    @api.multi
-    def create_ruler(self):
-
-        context = self.env.context
-        ruler_generator_id = self.ruler_generator_id
-
-        if context and ruler_generator_id:
-            self.env['rulers'].create(
-                {
-                    'name': "Temp Name!!",
-                    'generator_id': ruler_generator_id,
-                    'ruler_model': self.ruler_model.id,
-                    'ruler_field': self.ruler_field.id,
-                    'ruler_type': self.ruler_type,
-                    'logical_operator': self.logical_operator.id,
-                    'fields_type': self.fields_type,
-                    'date_value': self.date_value,
-                    'integer_value': self.integer_value,
-                    'bool_value': self.bool_value,
-                    'char_value': self.char_value,
-                    'float_value': self.float_value,
-                    'many2many_value': self.many2many_value,
-                }
-            )
-            return True
-        return False
